c67fb99ccaf92efb22fe4552570feeae34dc1f61
jose-marquez89 <jose_marquez89@outlook.com>
2020-07-03 15:10:53 -0500

    adds url components

--- python_scrape/indeed_scrape.py
+++ python_scrape/indeed_scrape.py
@@ -1,1 +1,8 @@
 # indeed_scrape.py - scrape major tech job listings from indeed_
+import requests
+from urllib.parse import urljoin
+
+base_url = "https://www.indeed.com"
+job_query = "jobs?q="
+loc_query = "&l="
+page_query = "&start="
