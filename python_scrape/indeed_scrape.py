22cdf02d8885aaeb45270da002384f8abcf2d81f
jose-marquez89 <jose_marquez89@outlook.com>
2020-07-05 17:47:55 -0500

    adds pseudocode for listing fetch function

--- python_scrape/indeed_scrape.py
+++ python_scrape/indeed_scrape.py
@@ -1,6 +1,10 @@
 # indeed_scrape.py - scrape major tech job listings from indeed_
-import requests
+import re
 from urllib import parse
+
+import requests
+from bs4 import BeautifulSoup
+
 
 
 def build_url(site, job, state, page):
@@ -27,7 +31,21 @@ def fetch_listings(job, state, base_url):
     """get all job listings in one state for one title"""
     initial_url = build_url(base_url, job, state, 0)
 
-    # TODO: get job counts from initial_url
+    res = requests.get(initial_url)
+    try:
+        res.raise_for_status()
+    except Exception as err:
+        print(err)
+
+    soup = BeautifulSoup(res.text)
+    # TODO: Because job counts appear to be unreliable
+    #       get page number. When page number is <= last
+    #       page number, stop getting next page
+
+    # TODO: use regular expression to get page number for iterations
+
+    # TODO: get details from job Card
+    # TODO: store details to csv
     pass
 
 
