dc5f0dc3756ce05512e4af44275a3389ed269c46
jose-marquez89 <jose_marquez89@outlook.com>
2020-07-13 08:05:37 -0500

    adds tests for latest functions

---
+++ python_scrape/test_functions.py
@@ -0,0 +1,21 @@
+import unittest
+import indeed_scrape as scrape
+
+
+class TestScrapeFunctions(unittest.TestCase):
+
+    def test_build_url(self):
+        url = scrape.build_url("indeed", "Data Scientist", "Texas", page=50)
+        expected = ("https://www.indeed.com/"
+                    "jobs?q=Data%20Scientist&l=Texas&start=50")
+        self.assertEqual(url, expected)
+
+    def test_fetch_page(self):
+        job_data = scrape.fetch_page_listings("Data Scientist", "Texas", "indeed")
+        self.assertNotEqual(len(job_data), 0)
+        self.assertIsInstance(job_data, list)
+        self.assertIsInstance(job_data[0], dict)
+
+
+if __name__ == '__main__':
+    unittest.main()
